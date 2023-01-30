8d7dc5b10517b1c1b5ceba6fd53f20e84ad44f43
gunal15 <gunalnandakumar@gmail.com>
2023-01-30 21:05:26 +0530

    db name

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-const dbName = 'webscrapping'
+const dbName = 'webscraper'
 const dbURL = `mongodb+srv://gunal15:C0o7vB5bNY9LZaJX@cluster0.orkw7uh.mongodb.net/${dbName}`;
 
 module.exports={dbURL,mongodb,MongoClient,dbName}
