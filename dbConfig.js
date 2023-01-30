4aa75403c06dd7a95c2f16dcab9fe22eb1343581
gunal15 <gunalnandakumar@gmail.com>
2023-01-30 17:33:37 +0530

    first commit

---
+++ dbConfig.js
@@ -0,0 +1,6 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const dbName = 'webscrapping'
+const dbURL = `mongodb+srv://gunal15:C0o7vB5bNY9LZaJX@cluster0.orkw7uh.mongodb.net/webscraper`;
+
+module.exports={dbURL,mongodb,MongoClient,dbName}
