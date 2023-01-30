be9b65dab9fb927d0437893cd92a99e3879dab9c
GUNAL N <113163255+gunal15@users.noreply.github.com>
2023-01-30 19:52:35 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 const dbName = 'webscrapping'
-const dbURL = `mongodb+srv://gunal15:C0o7vB5bNY9LZaJX@cluster0.orkw7uh.mongodb.net/${dbName}`;
+const dbURL = `https://cloud.mongodb.com/v2/63d25c188fe4ef3b0c5c54f6#/metrics/replicaSet/63d25ce498dc420fe2fba995/explorer/webscraper`;
 
 module.exports={dbURL,mongodb,MongoClient,dbName}
